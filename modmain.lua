89178070c3372d3f0c5b48bddb4390b71b46c042
chengzhi <chengzhi@kuaishou.com>
2023-01-29 11:28:24 +0800

    项目初始化

---
+++ modmain.lua
@@ -0,0 +1,61 @@
+local _G = GLOBAL
+
+-- 概率升级物品
+local useUpgradePolicy = GetModConfigData('UseUpgradePolicy')
+TUNING.useUpgradePolicy = useUpgradePolicy
+
+local useRoleLevelUp = GetModConfigData('UseRoleLevelUp')
+local useMorePick = GetModConfigData('UseMorePick')
+local useMoreDrop = GetModConfigData('UseMoreDrop')
+local useRoleStrenth = GetModConfigData('UseRoleStrenth')
+local useWeaponLevelUp = GetModConfigData('UseWeaponLevelUp')
+local useHatslLevelUp = GetModConfigData('UseHatslLevelUp')
+local useFarmLevelUp = GetModConfigData('UseFarmLevelUp')
+
+
+if useMorePick then
+	modimport("scripts/zaxmods/zaxmorepick.lua")
+end
+
+if useWeaponLevelUp then
+	modimport("scripts/zaxmods/zaxweapons.lua")
+end
+
+if useHatslLevelUp then
+	modimport("scripts/zaxmods/zaxhats.lua")
+end
+
+if useFarmLevelUp then
+	modimport("scripts/zaxmods/zaxfarm.lua")
+end
+
+
+-- 初始化角色，添加组件
+-- init character and add components
+local function initPalyer(inst)
+	-- 角色升级
+	if useRoleLevelUp then 			
+		inst:AddComponent("rolelevel")
+	end
+	-- 额外掉落
+	if useMoreDrop then 
+		inst:AddComponent("zxmoredrop")
+	end
+	-- 角色强化
+	if useRoleStrenth then
+		inst:AddComponent("rolestrenth")
+	end
+
+	if useFarmLevelUp then
+		inst:AddComponent("zxfarmskill")
+	end
+
+	inst.components.talker:Say("我又来到这个奇怪的世界！")	
+end
+
+
+if GLOBAL.TheNet:GetIsServer() then
+	AddPlayerPostInit(initPalyer)
+end
+
+
