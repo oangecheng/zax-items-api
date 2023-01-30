803e26e94bff4252c4b24700288e0d7b86413d2e
chengzhi <chengzhi@kuaishou.com>
2023-01-30 18:38:18 +0800

    修改modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -1,34 +1,18 @@
 local ch = locale == "zh" or locale == "zhr"
 
-local VERSION = "0.2.8"
+local VERSION = "0.1.0"
 
 -- 名称
-name = "Zax Level Up"
+name = ch and "建家党狂喜" or "more items"
 -- 描述
 description = 
 ch and 
-[[
-V 0.2.8
-你可以升级你的角色和物品，
-可以通过战斗击杀对方获得经验，
-可以吃下美味的食物获取经验，
-可以通过种植巨大作物提升自己的种田能力，
-可以通过给予物品升级女武神的长矛和普通长矛，
-可以通过给予物品升级海象帽，
-以上选项都可以通过设置自行选择开启还是关闭
-提醒: mod 角色如果自带升级不一定兼容哦！
+"version" .. VERSION .. [[
+	一些额外的物品，提升游戏体验
 ]]
 or 
-[[
-V 0.2.8
-You can upgrade your characters and items.
-You can gain experience by killing opponents in battle.
-You can eat delicious food to gain experience.
-You can improve your farming ability by planting huge crops
-Wathgrithr spears and normal spears can be upgraded by giving items.
-Walrushat can be upgraded by giving items.
-All of the above options can be turned on or off through the settings.
-Reminder: mod characters may not be compatible with their own upgrades!
+"version" .. VERSION .. [[
+	more items to improve game experience
 ]]
 
 
@@ -45,84 +29,13 @@ api_version = 10
 
 configuration_options = 
 {
-
 	{
-		name = "UseRoleLevelUp",
-		label = ch and "角色升级" or "upgrade character",
+		name = "zx_granary",
+		label = ch and "建造仓库" or "build granary",
 		options = {
 			{description = ch and "开启" or "enable", data = true},
 			{description = ch and "关闭" or "disable", data = false},
 		},
 		default = true,
-	},
-
-	{
-		name = "UseMorePick",
-		label = ch and "多倍采集" or "more pick",
-		options = {
-			{description = ch and "开启" or "enable", data = true},
-			{description = ch and "关闭" or "disable", data = false},
-		},
-		default = true,
-	},
-
-	{
-		name = "UseMoreDrop",
-		label = ch and "额外掉落" or "more item drop",
-		options = {
-			{description = ch and "开启" or "enable", data = true},
-			{description = ch and "关闭" or "disable", data = false},
-		},
-		default = true,
-	},
-
-	{
-		name = "UseRoleStrenth",
-		label = ch and "人物强化" or "strengthen character",
-		options = {
-			{description = ch and "开启" or "enable", data = true},
-			{description = ch and "关闭" or "disable", data = false},
-		},
-		default = true,
-	},
-
-	{
-		name = "UseWeaponLevelUp",
-		label = ch and "长矛/战斗长矛可升级" or "upgrade weapon" ,
-		options = {
-			{description = ch and "开启" or "enable", data = true},
-			{description = ch and "关闭" or "disable", data = false},
-		},
-		default = true,
-	},
-
-	{
-		name = "UseHatslLevelUp",
-		label = ch and "帽子可升级" or "upgrade hats" ,
-		options = {
-			{description = ch and "开启" or "enable", data = true},
-			{description = ch and "关闭" or "disable", data = false},
-		},
-		default = true,
-	},
-
-	{
-		name = "UseFarmLevelUp",
-		label = ch and "种地能力提升" or "upgrade farm skill" ,
-		options = {
-			{description = ch and "开启" or "enable", data = true},
-			{description = ch and "关闭" or "disable", data = false},
-		},
-		default = true,
-	},
-
-	{
-		name = "UseUpgradePolicy",
-		label = ch and "物品概率升级" or "item random upgrade",
-		options = {
-			{description = ch and "开启" or "enable", data = true},
-			{description = ch and "关闭" or "disable", data = false},
-		},
-		default = false,
-	},
+	}
 } 
