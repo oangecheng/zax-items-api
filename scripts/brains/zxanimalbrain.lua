3c1c3cdc7ed2e3a96008142026b142fbe5c7be60
zax <281725011@qq.com>
2024-03-27 23:46:16 +0800

    修复bug

--- scripts/brains/zxanimalbrain.lua
+++ scripts/brains/zxanimalbrain.lua
@@ -4,7 +4,7 @@ require "behaviours/leash"
 require "behaviours/doaction"
 local BrainCommon = require("brains/braincommon")
 
-local MAX_WANDER_DIST = ZXTUNING.FARM_AREA * 3.5
+local MAX_WANDER_DIST = ZXTUNING.FARM_AREA * 3.5 * 0.5
 
 
 local function getHostLoc(inst)
--- scripts/defs/animaldefs.lua
+++ scripts/defs/animaldefs.lua
@@ -11,6 +11,7 @@ end
 
 
 local perddata = {
+    sg = "SGperd",
     assets = ASSETSDEF.perd,
     anim = {
         bank = "perd",
