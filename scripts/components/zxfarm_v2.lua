d6b9fda29f5f8b315921d7d6c7c24ee5a826b3b6
chengzhi <chengzhi@kuaishou.com>
2024-03-27 10:57:46 +0800

    优化农场代码

--- scripts/components/zxfarm_v2.lua
+++ scripts/components/zxfarm_v2.lua
@@ -1,10 +1,12 @@
 
 
-
+---comment 计算当前动物产品总数量
+---@param self table farm组件
+---@return integer 数量
 local function productionsCnt(self)
     local sum = 0
     if self.productions then
-        for k, v in pairs(self.productions) do
+        for _, v in pairs(self.productions) do
             sum = sum + v
         end
     end
@@ -15,29 +17,58 @@ end
 local Farm = Class(function (self, inst)
     self.inst = inst
     self.childcount = 0
-    self.productions = nil
     self.childmax = 10
+    --- @type table|nil 
+    --- 存储产品 key = prefab, v = num
+    self.productions = nil
 end)
 
 
+---comment 设置动物上限
+---@param max number 上限
+function Farm:SetChildMaxCnt(max)
+    self.childmax = max or 10
+end
+
+
+---comment 获取动物上限值
+---@return number 上限
+function Farm:GetChildMaxCnt()
+    return self.childmax or 10
+end
+
+
+---comment 判断农场小动物是否满了
+---@return boolean
+function Farm:IsFull()
+    return self.childcount >= self.childmax
+end
+
+
+---comment 是否还能存放物品
+---@return boolean true可继续存放
 function Farm:CanStore()
     return productionsCnt(self) < 100
 end
 
 
+---comment 存放动物产物
+---@param prefab string 物品
+---@param num number 数量
 function Farm:Store(prefab, num)
     local productions = self.productions or {}
-    if self:CanStore() then
-        local newnum = (productions[prefab] or 0) + num
-        productions[prefab] = newnum
-        self.productions = productions
-        if not self:CanStore() then
-            self.inst:PushEvent("ZXPauseProduce", {})
-        end
+    local newnum = (productions[prefab] or 0) + num
+    productions[prefab] = newnum
+    self.productions = productions
+    if not self:CanStore() then
+        self.inst:PushEvent("ZXPauseProduce", {})
     end
 end
 
 
+---comment 收获动物产物
+---@param doer table 玩家
+---@return boolean 是否收获成功
 function Farm:Harvest(doer)
     if not (self.productions and doer.components.inventory) then
         return false
