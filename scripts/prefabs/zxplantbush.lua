2f5e0aa0320df10c39cf95d6ad356dbe072bbc22
chengzhi <chengzhi@kuaishou.com>
2024-05-10 16:58:17 +0800

    新增植物

---
+++ scripts/prefabs/zxplantbush.lua
@@ -0,0 +1,84 @@
+
+local bush_stages = {
+    {
+        name = "bud",
+        time = function () return TUNING.TOTAL_DAY_TIME end,
+        fn = function (inst, stage)
+            inst.AnimState:PlayAnimation("idle_stage"..stage)
+        end
+    },
+    {
+        name = "grow",
+        time = function () return TUNING.TOTAL_DAY_TIME end,
+        fn = function (inst, stage)
+            inst.AnimState:PlayAnimation("idle_stage"..stage)
+        end
+    },
+    {
+        name = "ripe",
+        time = function () return TUNING.TOTAL_DAY_TIME end,
+        fn = function (inst, stage)
+            inst.AnimState:PlayAnimation("idle_stage"..stage)
+        end
+    }
+}
+
+
+local function onDigFinished(inst, worker)
+    inst.components.lootdropper:SpawnLootPrefab(inst.prefab.."_dug")
+    inst:Remove()
+end
+
+
+
+local function MakePlantBush(prefab, data)
+
+    local prefabs = {}
+
+    local assets = {}
+    local res = ZxGetPrefabAnimAsset(prefab)
+    for i, v in ipairs(res) do
+        table.insert(assets, v)
+    end
+
+    local function fn()
+        local inst = CreateEntity()
+        inst.entity:AddTransform()
+        inst.entity:AddAnimState() 
+        inst.entity:AddMiniMapEntity()
+        inst.entity:AddNetwork()
+        inst.entity:SetPristine()
+        MakeSmallObstaclePhysics(inst, .1)
+
+        ZxInitItemForClient(inst, prefab, "stage_1")
+        if not TheWorld.ismastersim then
+            return inst
+        end
+        ZxInitItemForServer(inst, prefab)
+
+        inst:AddComponent("lootdropper")
+        inst:AddComponent("inspectable")
+
+        inst:AddComponent("workable")
+        inst.components.workable:SetWorkAction(ACTIONS.DIG)
+        inst.components.workable:SetWorkLeft(1)
+        inst.components.workable:SetOnFinishCallback(onDigFinished)
+
+        inst:AddComponent("growable")
+        inst.components.growable.stages = bush_stages
+        inst.components.growable:SetStage(1)
+
+        -- 野火
+        inst:AddComponent("witherable")
+        MakeMediumBurnable(inst)
+        MakeMediumPropagator(inst)
+
+
+        return Prefab(prefab, fn, assets, prefabs)
+    end
+end
+
+
+return 
+
+
